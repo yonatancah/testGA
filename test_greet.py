05f8389a9547c2fe67ad3a717735afbcbb5eef6e
Yonatan Hutabarat <37174501+yonatancah@users.noreply.github.com>
2024-02-01 15:05:51 +0100

    Update test_greet.py

--- test_greet.py
+++ test_greet.py
@@ -4,3 +4,4 @@ def test_greet():
   assert greet('iBOTS') == "Hello, iBOTS!"
   assert greet('Sourav') == "Hello, Sourav!"
   assert greet('Yonatan') == "Hello, Yonatan!"
+  assert greet('Yonatan') != "Hello, Sourav!"
